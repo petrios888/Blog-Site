77bb6a186056b6c72886f05883cfd5baee2933c3
petrios888 <appeter22@yahoo.com>
2017-01-08 00:06:55 -0500

    First commit

---
+++ mysite/profiles/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+	context = {}
+	template = 'home.html'
+	return render(request,template,context)
+
+def about(request):
+	context = {}
+	template = 'about.html'
+	return render(request,template,context)
+
+
+# Create your views here.
