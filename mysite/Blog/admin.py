9317cc350bb1ac539dbddf8061764f42cef12e10
petrios888 <petrios888@users.noreply.github.com>
2017-01-12 15:47:07 -0500

    Add files via upload

---
+++ mysite/Blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from Blog.models import Post
+
+admin.site.register(Post)
+
+
+
+
+
+
+
+# Register your models here.
---
+++ mysite/Blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+
+#def Blog(request):
+#	context = {}
+#	template = 'Blog.html'
+#	return render(request,template,context)
+	
+	
+#def Blog(request):
+#	context = {}
+#	template = 'post.html'
+#	return render(request,template,context)
+
+	
+	
+# Create your views here.
